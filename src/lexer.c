8f2046fdaa5b3e2d34a091bf7be16d7b291b2dcd
GruelingPine185 <66073333+GruelingPine185@users.noreply.github.com>
2023-08-06 22:29:36 -0400

    handle whitespace

--- src/lexer.c
+++ src/lexer.c
@@ -11,6 +11,21 @@ struct calc_lexer {
 };
 
 
+static int calc_is_whitespace(int _c);
+
+
+static int calc_is_whitespace(int _c) {
+    return (
+        (_c == '\n') || (_c == '\t') || _c == (_c == '\r') ||
+        (_c == ' ')
+    );
+}
+
+static void calc_lexer_skip_whitespace(calc_lexer* _lexer) {
+    do {
+        if(!calc_is_whitespace(calc_lexer_curr(_lexer))) return;
+    } while(calc_lexer_advance(_lexer));
+}
 calc_lexer* calc_create_lexer(const char* _buffer) { 
     assert(_buffer != NULL);
     calc_lexer* lexer = (calc_lexer*) malloc(sizeof(*lexer));
@@ -25,4 +40,13 @@ void calc_destroy_lexer(calc_lexer* _lexer) {
     assert(_lexer != NULL);
     free(_lexer);
     _lexer = NULL;
+}
+
+calc_token* calc_lexer_lex(calc_lexer* _lexer) {
+    assert(_lexer != NULL);
+    if(calc_is_whitespace(calc_lexer_curr(_lexer))) {
+        calc_lexer_skip_whitespace(_lexer);
+    }
+
+    return NULL;
 }
