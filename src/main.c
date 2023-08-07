a6142f22e600d94529791ef529dd08eb3db1dec9
GruelingPine185 <66073333+GruelingPine185@users.noreply.github.com>
2023-08-06 22:25:06 -0400

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "lexer.h"
+
+
+static void destroy_tok(calc_token* _token);
+
+
+static void destroy_tok(calc_token* _token) {
+    free(_token->lexeme);
+    free(_token);
+    _token = NULL;
+}
+
+
+int main(int _argc, char** _argv) {
+    if(_argc == 1) return 1;
+
+    calc_lexer* lexer = calc_create_lexer(_argv[1]);
+    if(!lexer) return 1;
+
+    calc_token* tok = NULL;
+    while((tok = calc_lexer_lex(lexer))) {
+        if(!tok) break;
+
+        printf("offset: %u, type: %d, val: \'%s\'\n",
+                tok->offset,
+                tok->type,
+                tok->lexeme);
+        destroy_tok(tok);
+    }
+
+    calc_destroy_lexer(lexer);
+    return 0;
+}
